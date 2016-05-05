eb402250cd5801832fa3ba1f6be9f0dc5b03289c
un5t0ppab13 <devbrights@gmail.com>
2016-05-05 14:10:37 -0700

    Change method call to directions

--- lib/geminate.rb
+++ lib/geminate.rb
@@ -1,5 +1,6 @@
 class Geminate
-	def self.hi
-		puts "Geminate, the gem I made!"
+	def self.how
+		puts "Geminate: the gem I made to teach you how to build your own gems!"
+		puts "Learn how at: https://github.com/un5t0ppab13/geminate"
 	end
 end
